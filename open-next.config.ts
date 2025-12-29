ad5b5b2531ef908ef2f578640387789cc10d24fd
rabbikraz <rabbikraz@github.com>
2025-12-29 17:04:19 -0500

    Fix OpenNext config format

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,19 +5,22 @@ const config: OpenNextConfig = {
         override: {
             wrapper: "cloudflare-node",
             converter: "edge",
-            // Enable incremental cache with KV
+            proxyExternalRequest: "fetch",
             incrementalCache: "dummy",
             tagCache: "dummy",
             queue: "dummy",
         },
     },
-
+    edgeExternals: ["node:crypto"],
     middleware: {
         external: true,
         override: {
             wrapper: "cloudflare-edge",
             converter: "edge",
             proxyExternalRequest: "fetch",
+            incrementalCache: "dummy",
+            tagCache: "dummy",
+            queue: "dummy",
         },
     },
 };
